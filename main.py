80b950f7869df8196fa98f16f661a60bd1d548da
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2024-09-22 14:03:07 +1000

    intial commit. setup flask server

---
+++ main.py
@@ -0,0 +1,22 @@
+# setup flask file
+from flask import Flask
+import flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "This is the energy monitor server!"
+
+
+@app.route("/receive_data", methods=["POST"])
+def receive_data():
+    if flask.request.method == "POST":
+        data = flask.request.get_json()
+        print(data)
+        return "Data received"
+
+
+if __name__ == "__main__":
+    app.run()
