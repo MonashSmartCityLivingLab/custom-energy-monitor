ab75baffd09e76cee29b221bd2edc104e458ec5d
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2024-10-04 16:31:23 +1000

    updated esp file

--- client/esp/esp.ino
+++ client/esp/esp.ino
@@ -4,8 +4,9 @@
 #include <ArduinoJson.h>
 
 const char* ssid = "scll-1";
-const char* password =  "scll-2023#";
-String url = "http://192.168.68.101:5001/receive_data";
+const char* password = "scll-2023#";
+String baseUrl = "http://192.168.68.101:5001";
+String receiveDataEndpoint = baseUrl + "/receive_data";
 
 void setup() {
   Serial.begin(9600);  // Start serial communication
@@ -24,80 +25,111 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
+String createPayload(char* message, unsigned long timeDifference) {
+  JSONDocument doc;
+  doc["message"] = message;
+  doc["time_difference"] = timeDifference;
+  String payload;
+  serializeJson(doc, payload);
+  return payload;
+}
+
+struct ServerResponse {
+  int httpCode;
+  bool sendSuccess;
+  String message;
+}
+
+/**
+Input:
+  WifiClient client:
+  String fullUrl:
+      eg: http://example.com, http://192.168.68.101:5001/receive_data
+  String payload: 
+      This is a json payload which has already been serialised into a string. 
+      Allowing custom header may be allowed in the future but for now it has to be a json string
+
+@output
+*/
+ServerResponse
+sendDataTo(WiFiClient client, String fullUrl, String payload, bool verboseLogging = true) {
+  ServerResponse serverResponse;
+
+  if (http.begin(client, fullUrl)) {
+    http.addHeader("Content-Type", "application/json");
+
+    int httpCode = http.POST(payload);
+    serverResponse.httpCode = httpCode;
+
+    if (httpCode > 0) {
+      String response = http.getString();
+      serverResponse.message = response;
+      serverResponse.sendSuccess = true;
+    } else {
+      serverResponse.message = "tried to send data but some error occurred";
+      serverResponse.sendSuccess = false;
+    }
+
+    if (verboseLogging) {
+      Serial.println("[HTTP RESPONSE CODE]" + String(httpCode));
+      Serial.println("[SERVER RESPONSE]" + serverResponse.message);
+    }
+
+    http.end();
+  } else {
+    if (verboseLogging) {
+      Serial.println("[CONNEcTION ERROR] Unable to connect");
+    }
+    serverResponse.message = "error occurred";
+    serverResponse.sendSuccess = false;
+  }
+
+  return serverResponse;
+}
+
+String getValue(String data, char separator, int index) {
+  int found = 0;
+  int strIndex[] = { 0, -1 };
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
     // Check if serial data is available
     if (Serial.available() > 0) {
       // 1. Read the state (1 byte)
       int sensorState = Serial.read();  // Read the first byte (ON/OFF state)
-      
-      JsonDocument doc;
 
       // 2. Read the message (until newline or a known terminator)
       String message = "";  // to store any string messages like "State changed"
       while (Serial.available() > 0) {
         char c = Serial.read();
         if (c == '\n') break;  // Stop reading the message at a newline
-        message += c;  // Accumulate message characters
+        message += c;          // Accumulate message characters
       }
 
-      // 3. Read the time difference (4 bytes for unsigned long)
-      unsigned long timeDifference = 0;
-      if (Serial.available() >= 4) {  // Check if there are 4 bytes to read
-        byte timeBytes[4];  // Store the 4 bytes for time
-        for (int i = 0; i < 4; i++) {
-          timeBytes[i] = Serial.read();  // Read each byte
-        }
-        // Convert bytes to unsigned long
-        timeDifference = *((unsigned long*)timeBytes);  // Convert the byte array back to an unsigned long
-      }
-
-      // Print the received data for debugging purposes
-      // Serial.println("Received sensor state: " + String(sensorState == 1 ? "ON" : "OFF"));
-      // Serial.println("Message: " + message);
-      // Serial.println("Time difference: " + String(timeDifference));
+      // 3. Decode received string -> split by " " and the item after [SEND] is the time difference
+      unsigned long timeDifference = (unsigned long) getValue(message, " ", 1);
+      String payload = createPayload("sending", timeDifference);
 
       // Send the data to your server
       WiFiClient client;
       HTTPClient http;
-      String fullUrl = url;
 
-      if (http.begin(client, fullUrl)) {
-        http.addHeader("Content-Type", "application/json");
-
-        // Prepare the JSON payload with the received data
-        // String jsonPayload = "{\"sensor_state\":\"" + String(sensorState == 1 ? "ON" : "OFF") + "\",";
-        // jsonPayload += "\"message\":\"" + message + "\",";
-        // jsonPayload += "\"time_difference\":" + String(timeDifference) + "}";
-        doc["message"] = message;
-        doc["time_difference"] = String(timeDifference);
-
-        // Create a String to hold the JSON data
-        String payload;
-        serializeJson(doc, payload);
-
-        // Debug: Print the JSON payload to Serial
-        Serial.println("Serialized JSON: " + payload);
-
-        // Send the POST request with the serialized JSON data
-        int httpCode = http.POST(payload);
-        
-        // Print the HTTP response code
-        Serial.println("HTTP Response Code: " + String(httpCode));
-
-        // Print the server response
-        if (httpCode > 0) {
-          String response = http.getString();
-          Serial.println("Server Response: " + response);
-        }
-
-        http.end();  // Close the connection
-      } else {
-        Serial.println("[HTTP] Unable to connect");
+      ServerResponse postRequest = sendDataTo(client, receiveDataEndpoint, payload, true);
+      if (postRequest.sendSuccess) {
       }
     }
-  } else {
-    Serial.println("WiFi Disconnected");
+
+    delay(1000);
   }
-  delay(1000);
-}
